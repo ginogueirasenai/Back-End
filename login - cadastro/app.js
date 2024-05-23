a222484f42f9d70c6fa4f5f1a0fb53c7812569b9
ginogueirasenai <ginogueira.senai@gmail.com>
2024-05-23 16:02:20 -0300

    Atualização de verificacao login-cadastro

--- login - cadastro/app.js
+++ login - cadastro/app.js
@@ -3,44 +3,53 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000; 
+const port = 3000;
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'giovanna',
+    host: 'localhost',
+    user: 'giovanna',
     password: 'SENAI123',
     database: 'login2'
 });
 
-db.connect((error)=>{
-    if (error){
-        console.log("Erro ao conectar com o banco de dados")
-    }else {
+db.connect((error) => {
+    if (error) {
+        console.log("Erro ao conectar com o banco de dados");
+    } else {
         console.log("Conectado ao MySQL");
     }
 });
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname + '/login.html')
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
 
-app.post("/login", (req, res)=>{
-    const username = req.body.usuario
-    const password = req.body.senha
+app.post("/login", (req, res) => {
+    const username = req.body.usuario;
+    const password = req.body.senha;
 
-    db.query('SELECT password FROM user WHERE username = ?', [username], (error, results)=>{
-        if (results.length > 0) {
-        const passwordBD = results[0].password;
-        console.log(passwordBD)
-        }else{
-            console.log('Usuário não cadastrado!')
+    db.query('SELECT password FROM user WHERE username = ?', [username], (error, results) => {
+        if (error) {
+            console.log("Erro na consulta ao banco de dados:", error);
+            res.status(500).send("Erro interno do servidor");
+        } else if (results.length > 0) {
+            const passwordBD = results[0].password;
+            if (password === passwordBD) {
+                console.log("Login bem-sucedido!");
+                res.send("Login bem-sucedido!");
+            } else {
+                console.log("Senha incorreta!");
+                res.send("Senha incorreta!");
+            }
+        } else {
+            console.log("Usuário não cadastrado!");
+            res.send("Usuário não cadastrado!");
         }
-    })  
-})
+    });
+});
 
-
-app.listen(port, ()=>{
-    console.log(`Servidor rodando no endereco http://localhost: ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${port}`);
+});
